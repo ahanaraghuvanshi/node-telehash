e2012bd02d355866119a459b9a6fb59a494b05b8
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2012-04-11 01:47:13 +0300

    beautified code, added comments and thoughts

--- client.js
+++ client.js
@@ -1,24 +1,33 @@
 var telehash = require("./telehash");
 var hlib = require("./hash");
 
-telehash.seed( function(err){
-        if ( err ){
-                console.log(err);
-                return;
-	}
-	connect("echo.message.back");
+telehash.seed(function (err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    connect("echo.message.back");
 });
 
-function connect(name){
-	telehash.connect({id:name, message:'telehash rocks!'}, function(s,telex){			
-		
+//once we are seeded. we connect to switches listening for 'name' and send them a message
+//expecting a response. As this connection goes is relayed over the network they may receive it multiple times
+//through the various switches they are tapping. When we receive a response from the switch
+//we simply log it to the console.
+//The connect process is continues.  the message will be sent out multiple times/minute and equally responses 
+//will arrive continiously
+function connect(name) {
+    telehash.connect({
+        id: name,
+        message: 'telehash rocks!'
+    }, function (s, telex) {
+
         //we got a direct data telex back. We can send telexes back now with telehash.send(s.ipp, {...});
-		if(telex['message'])  console.log("Reply MESSAGE: ", telex['message'],"from:",s.ipp );
+        if (telex['message']) console.log("Reply MESSAGE: ", telex['message'], "from:", s.ipp);
 
-        //we are behind an SNAT so we get a relayed telex back.. we need to negotiate another way to connect
-        //STUN,TURN, ICE... or roll our own?
-   		if(telex['+message']) console.log("Reply MESSAGE: ", telex['+message'],"relay from:",s.ipp );
+        //we are behind an SNAT, or the other end is behind the same NAT so we get a relayed telex back.. 
+        //we need to negotiate another way to connect
+        //STUN,TURN, ICE... or roll our own? or even directly through local LAN if we are behind same NAT.
+        if (telex['+message']) console.log("Reply MESSAGE: ", telex['+message'], "relay from:", s.ipp);
 
-	});
+    });
 }
-
