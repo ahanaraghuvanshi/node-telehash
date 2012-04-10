f8b060369a66060e2cba70b4e3c6aa62f58a3a59
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2012-04-11 01:05:20 +0300

    beautified code, added some comments.

--- channel-client.js
+++ channel-client.js
@@ -1,28 +1,29 @@
 var channels = require('./channels');
 
-channels.init({
-	//seeds:['172.16.200.253:42424'],
-	ready:function(){
-		connect();
-	}		
+channels.init({    
+    ready: function () { //will be called when we are seeded    	
+        connect();
+    }
 });
 
-function connect(){
-
-	channels.connect("telehash.echo.server", onConnect );
+function connect() {
+    //this will establish a connection to any switche(s) on the network
+    //listening for 'telehash.echo.server'. onConnect will be called for each remote switch
+    //which accepts the connection request.
+    channels.connect("telehash.echo.server", onConnect);    
 }
 
-function onConnect( server ){
+function onConnect(server) {
+    //when a connection is made we get back a server object we can use to communicate with the
+    //remote switch
+    console.log("CONNECTED");
+    server.data = function (msg) {
+        //this is a triggerred callback for when we receive a datagram from the remote switch
+        console.log("data from server: " + msg.toString());
+    }
 
-	console.log("CONNECTED");
-	server.data = function(msg){
-
-		console.log("data from server: " + msg.toString() );
-	}
-
-	setInterval( function(){				
-		server.send( new Buffer("Hello!") );
-	},5000);
+    setInterval(function () {
+    	//the send method in the server object is used to send a datagram to the remote switch
+        server.send(new Buffer("Hello!"));
+    }, 5000);
 }
-
-
