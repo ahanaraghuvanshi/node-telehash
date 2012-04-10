f7ec4edc9df742aa9a2b8bfec28bc0ade14ef42f
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2012-04-11 01:51:32 +0300

    beautified code

--- seed.js
+++ seed.js
@@ -3,9 +3,11 @@ var util = require('./util');
 
 var ip = util.getLocalIP();
 
-if(ip.length > 0 ) {
-  var seeds = [ ip[0]+":42424" ];//use the first ip-address as our seed ip
-  telehash.init( {port:'42424', seeds:seeds} );
-  telehash.seed();
+if (ip.length > 0) {
+    var seeds = [ip[0] + ":42424"]; //use the first ip-address as our seed ip
+    telehash.init({
+        port: '42424',
+        seeds: seeds
+    });
+    telehash.seed();
 }
-
