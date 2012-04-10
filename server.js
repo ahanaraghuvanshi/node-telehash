6fcb02bc94004c89b0ba92fd504351a84cdad704
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2012-04-11 01:53:17 +0300

    beautified code.

--- server.js
+++ server.js
@@ -2,27 +2,34 @@ var telehash = require("./telehash");
 var hlib = require("./hash");
 var util = require("./util");
 
-telehash.seed( function(err){
-        if ( err ){
-                console.log(err);
-                return;
-	}
-	server("echo.message.back");
+telehash.seed(function (err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    server("echo.message.back");
 });
 
-function server(name){
-	telehash.listen({id:name}, function(s,telex){				
-		console.log("Incoming telex:"+JSON.stringify(telex)+" via:"+s.ipp);
-		console.log("MESSAGE:",telex['+message']);
+function server(name) {
+    telehash.listen({
+        id: name
+    }, function (s, telex) {
+        console.log("Incoming telex:" + JSON.stringify(telex) + " via:" + s.ipp);
+        console.log("MESSAGE:", telex['+message']);
 
-		//if remote end is behind SNAT or we are behind the same NAT send back via relay
-		if(telex['+snat'] || util.IP(telex['+from']) == util.IP(telex._to)){
-
-			var end = new hlib.Hash(telex['+from']).toString();	
-		    s.send( {'+end':end,'+message':telex['+message'],'+connect':telex['+connect']} );//signals to be relayed back
-
-		}else{
-			telehash.send( telex['+from'], {'message':telex['+message'],'connect':telex['+connect']});//data telex
-		}
-	});
+        //if remote end is behind SNAT or we are behind the same NAT send back via relay
+        if (telex['+snat'] || util.IP(telex['+from']) == util.IP(telex._to)) {
+            var end = new hlib.Hash(telex['+from']).toString();
+            s.send({
+                '+end': end,
+                '+message': telex['+message'],
+                '+connect': telex['+connect']
+            }); //signals to be relayed back
+        } else {
+            telehash.send(telex['+from'], {
+                'message': telex['+message'],
+                'connect': telex['+connect']
+            }); //data telex
+        }
+    });
 }
