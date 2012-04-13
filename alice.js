8cc0f568dd40bffd0a3cb3e4b17cad729a0400ad
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2012-04-14 00:56:40 +0300

    a p2p example of using channels

---
+++ alice.js
@@ -0,0 +1,30 @@
+var channels = require('../channels');
+
+channels.init({
+    ready: function () { //will be called when we are seeded    	
+        connect();
+    }
+});
+
+function connect() {
+   
+    channels.listen("@alice", function(peer){onConnect(peer,"@alice");});
+    channels.connect("@bob", function(peer){onConnect(peer,"@bob");});
+    channels.connect("@eve", function(peer){onConnect(peer,"@eve");});
+}
+
+function onConnect(peer, User ) {
+    if(User == "@alice") {
+        console.log("Incoming Connection.");
+    }else{
+        console.log("Connected to:", User);
+    }
+
+    setInterval(function(){
+        peer.send( new Buffer("Hi, I'm ALICE!"));
+    },3000);
+    
+    peer.data = function (msg) {
+        console.log("<- ",peer.id," ", msg.toString() );
+    }  
+}
