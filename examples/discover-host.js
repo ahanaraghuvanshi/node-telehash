7f1d51a4a535d0f341e174501617f3a7121ad0e7
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-01-19 17:10:56 +0200

    debug to console in discover examples

--- examples/discover-host.js
+++ examples/discover-host.js
@@ -1,16 +1,17 @@
 var telehash = require("../index.js").v1.telehash;
 var util = require('../lib/v1/iputil');
-var NETWORK_INTERFACE = "";//for example eth0, zt0
+var NETWORK_INTERFACE = ""; //for example eth0, zt0
 
 telehash.init({
-    mode:3,         // full operating mode
-    interface: NETWORK_INTERFACE,
-    udplib:"enet",
-    respondToBroadcasts:true,
-    port:42424,
-    onSocketBound:function(addr){
-        console.log("bound to address:",addr);
-        console.log("listening for broadcasts");
-        telehash.broadcast();
-    }
+	log: console.error,
+	mode: 3, // full operating mode
+	interface: NETWORK_INTERFACE,
+	udplib: "node",
+	respondToBroadcasts: true,
+	port: 42424,
+	onSocketBound: function (addr) {
+		console.log("bound to address:", addr);
+		console.log("listening for broadcasts");
+		telehash.broadcast();
+	}
 });
--- examples/discover.js
+++ examples/discover.js
@@ -1,18 +1,19 @@
 var telehash = require("../index.js").v1.telehash;
 var util = require('../lib/v1/iputil');
-var NETWORK_INTERFACE = "";//for example eth0, zt0 - if empty first interface with non loopack address found will be used
+var NETWORK_INTERFACE = ""; //for example eth0, zt0 - if empty first interface with non loopack address found will be used
 
 var bcast = process.argv[2] == 'broadcast';
 
 telehash.init({
-    mode:3,         // full operating mode
-    interface: NETWORK_INTERFACE,
-    udplib:"enet",
-    onSocketBound:function(addr){
-        console.log("bound to address:",addr);
-        if(bcast){
-            console.log("broadcasting...");
-            telehash.broadcast();
-        } else telehash.seed();
-    }
+	log: console.error,
+	mode: 3, // full operating mode
+	interface: NETWORK_INTERFACE,
+	udplib: "enet",
+	onSocketBound: function (addr) {
+		console.log("bound to address:", addr);
+		if (bcast) {
+			console.log("broadcasting...");
+			telehash.broadcast();
+		} else telehash.seed();
+	}
 });
