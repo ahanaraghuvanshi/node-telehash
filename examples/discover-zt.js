1d844d4a73be4da3fba7d266bc52224b7acb3226
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-01-19 17:10:10 +0200

    updated seed ip on zerotier

--- examples/discover-zt.js
+++ examples/discover-zt.js
@@ -4,15 +4,16 @@ var NETWORK_INTERFACE = "zt0"; //tun interface created by zerotier
 var bcast = process.argv[2] == 'broadcast';
 
 telehash.init({
-    mode:3,         // full operating mode
-    interface: NETWORK_INTERFACE,
-    seeds: ["28.192.75.207:42424"],
-    udplib:"enet",
-    onSocketBound:function(addr){
-        console.log("bound to address:",addr);
-        if(bcast){
-            console.log("broadcasting...");
-            telehash.broadcast();
-        } else telehash.seed();
-    }
+	log: console.error,
+	mode: 3, // full operating mode
+	interface: NETWORK_INTERFACE,
+	seeds: ["28.192.75.206:42424"],
+	udplib: "enet",
+	onSocketBound: function (addr) {
+		console.log("bound to address:", addr);
+		if (bcast) {
+			console.log("broadcasting...");
+			telehash.broadcast();
+		} else telehash.seed();
+	}
 });
